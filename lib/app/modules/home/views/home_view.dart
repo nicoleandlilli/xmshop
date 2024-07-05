238898448aa7b557591bfd12bb382cc3e55c674f
wangguiye <wangguiye@lenovo.com>
2024-07-05 21:13:05 +0800

    屏幕适配

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:xmeshop/app/services/kee_alive_wrapper.dart';
 
@@ -9,12 +10,21 @@ class HomeView extends GetView{
 
   @override
   Widget build(BuildContext context) {
-    return const KeepAliveWrapper(
+    return KeepAliveWrapper(
         child: Scaffold(
-          body: Center(
-            child:  Text("HomeView"),
+          body: Column(
+            children: [
+              Container(
+                width: 1080.w,
+                height: 690.h,
+                color: Colors.red,
+                child: const Center(
+                  child:  Text("HomeView"),
+                ),
+              )
+            ],
           ),
-        )
+        ),
     );
   }
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:xmeshop/app/routes/app_pages.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
-  runApp(GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.initial,
-      getPages: AppPages.routes,
-  ),);
+  runApp(
+    ScreenUtilInit(
+      designSize: const Size(1080, 2400),//设计稿的宽度和高度px
+      minTextAdapt: true,
+      splitScreenMode: true,
+      // Use builder only if you need to use library outside ScreenUtilInit context
+      builder: (context , child) {
+        return GetMaterialApp(
+          title: "Application",
+          initialRoute: AppPages.initial,
+          getPages: AppPages.routes,
+        );
+      },
+    ),
+
+  );
 }
