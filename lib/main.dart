3fd9778a9be400609f021406c32f2b847074af49
wangguiye <wangguiye@lenovo.com>
2024-07-05 21:35:04 +0800

    去掉debug banner 状态栏设置为透明

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:flutter/services.dart';
 import 'package:xmeshop/app/routes/app_pages.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
 void main() {
+  //配置透明的状态栏
+  SystemUiOverlayStyle systemUiOverlayStyle = const SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+  SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
   runApp(
     ScreenUtilInit(
       designSize: const Size(1080, 2400),//设计稿的宽度和高度px
@@ -12,6 +16,7 @@ void main() {
       // Use builder only if you need to use library outside ScreenUtilInit context
       builder: (context , child) {
         return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
           title: "Application",
           initialRoute: AppPages.initial,
           getPages: AppPages.routes,
