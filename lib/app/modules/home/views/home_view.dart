c1bac50dc99db2a8206562515da5ba34e3322f70
wangguiye <wangguiye@lenovo.com>
2024-07-07 15:25:38 +0800

    将listview展示位置向上平移到状态栏下

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -17,33 +17,42 @@ class HomeView extends GetView<HomeController>{
         child: Scaffold(
           body: Stack(
             children: [
-              ListView.builder(
-                controller: controller.scrollController, //绑定controller
-                itemCount: 30,
-                itemBuilder: (context, index){
-                 if(index==0){
-                   return SizedBox(
-                     width: ScreenAdapter.width(1000),
-                     height: ScreenAdapter.width(682),
-                     child: Image.network("https://www.itying.com/images/focus/focus02.png",fit: BoxFit.cover,),
-                   );
-                 }else{
-                   return ListTile(title: Text("第$index个列表"),);
-                 }
-                }),
+              Positioned(
+                top: -60,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                child: ListView.builder(
+                    controller: controller.scrollController, //绑定controller
+                    itemCount: 30,
+                    itemBuilder: (context, index){
+                      if(index==0){
+                        return SizedBox(
+                          width: ScreenAdapter.width(1000),
+                          height: ScreenAdapter.width(682),
+                          child: Image.network("https://www.itying.com/images/focus/focus02.png",fit: BoxFit.cover,),
+                        );
+                      }else{
+                        return ListTile(title: Text("第$index个列表"),);
+                      }
+                    }),
+              ),
+
               Positioned(
                 top: 0,
                 left: 0,
                 right: 0,
                 child: Obx(()=> AppBar(
                   leading: controller.flag.value? const Text(""): const Icon(NCFonts.xiaomi, color: Colors.white,),
-                  title: Container(
-                    width: ScreenAdapter.width(620),
+                  leadingWidth: controller.flag.value?ScreenAdapter.width(40):ScreenAdapter.width(140),
+                  title: AnimatedContainer(
+                    width: controller.flag.value?ScreenAdapter.width(800):ScreenAdapter.width(620),
                     height: ScreenAdapter.height(96),
                     decoration: BoxDecoration(
                       color: const Color.fromARGB(230, 252, 243, 236),
                       borderRadius: BorderRadius.circular(30),
                     ),
+                    duration: const Duration(milliseconds: 300),
                     child: Row(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
