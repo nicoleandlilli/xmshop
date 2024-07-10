dca6290f00f8ea9a40f01a90fd14ff87e1ec8495
wangguiye <wangguiye@lenovo.com>
2024-07-11 06:01:59 +0800

    实现分类界面左侧点击listview条目时，指示器变更

--- lib/app/modules/category/controllers/category_controller.dart
+++ lib/app/modules/category/controllers/category_controller.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:get/get_rx/get_rx.dart';
 
 class CategoryController extends GetxController{
-  // RxInt currentIndex = 1.obs;
-  // PageController pageController=PageController(initialPage: 1);
-  // final List<Widget> pages=const[
-  //
-  // ];
+
+  RxInt selectIndex = 0.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+
+  void changeIndex(index){
+    selectIndex.value=index;
+    update();
+  }
 
 }
--- lib/app/modules/category/views/category_view.dart
+++ lib/app/modules/category/views/category_view.dart
@@ -13,41 +13,89 @@ class CategoryView extends GetView<CategoryController>{
   Widget build(BuildContext context) {
     return  KeepAliveWrapper(
         child: Scaffold(
-          appBar: AppBar(
-            title:  AnimatedContainer(
-              width: ScreenAdapter.width(840),
-              height: ScreenAdapter.height(96),
-              decoration: BoxDecoration(
-                color: const Color.fromRGBO(246, 246, 246, 1),
-                borderRadius: BorderRadius.circular(30),
-              ),
-              duration: const Duration(milliseconds: 300),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Padding(padding: EdgeInsets.fromLTRB(10, 0, 4, 0), child: Icon(Icons.search),),
-                  Text("手机",style: TextStyle(color: Colors.black54, fontSize: ScreenAdapter.fontSize(32)),),
-                ],
-              ),
-            ),
-            actions: [
-              IconButton(
-                  onPressed: (){
-
-                  },
-                  icon: const Icon(Icons.message_outlined, color: Colors.black45,)),
-
-            ],// 部阴影
-            centerTitle: true,
-            backgroundColor: Colors.white,
-            elevation: 0,
-          ),
-          body: Center(
-            child:  Text("CategoryView"),
-          ),
+          appBar: _buildAppBar(),
+          body: _buildBody(),
         )
     );
 
+  }
+
+  Widget _buildBody() {
+    return Row(
+      children: [
+        Container(
+          width: ScreenAdapter.width(280),
+          height: double.infinity,
+          child: ListView.builder(
+            itemCount: 20,
+              itemBuilder: (context, index){
+                return Container(
+                  width: double.infinity,
+                  height: ScreenAdapter.height(140),
+                  child: Obx(()=>InkWell(
+                    onTap: (){
+                      controller.changeIndex(index);
+                    },
+                    child: Stack(
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child:Container(
+                            width: ScreenAdapter.width(10),
+                            height: ScreenAdapter.height(46),
+                            color: controller.selectIndex.value==index?Colors.red:Colors.white,
+                          ),
+                        ),
+                        Center(
+                          child: Text("第$index个"),
+                        ),
+
+
+                      ],
+                    ),
+                  )),
+                );
+              },
+          ),
+        ),
+        Expanded(child: Container(
+          height: double.infinity,
+          color: Colors.black,
+        ),),
+      ],
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+          title:  AnimatedContainer(
+            width: ScreenAdapter.width(840),
+            height: ScreenAdapter.height(96),
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(246, 246, 246, 1),
+              borderRadius: BorderRadius.circular(30),
+            ),
+            duration: const Duration(milliseconds: 300),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Padding(padding: EdgeInsets.fromLTRB(10, 0, 4, 0), child: Icon(Icons.search),),
+                Text("手机",style: TextStyle(color: Colors.black54, fontSize: ScreenAdapter.fontSize(32)),),
+              ],
+            ),
+          ),
+          actions: [
+            IconButton(
+                onPressed: (){
+
+                },
+                icon: const Icon(Icons.message_outlined, color: Colors.black45,)),
+
+          ],// 部阴影
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          elevation: 0,
+        );
   }
 
 }
