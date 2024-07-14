2ac681d58835d3280d59ec962cd1cf478d7b3583
nicole <wangguilixiadong.163.com>
2024-07-15 00:53:47 +0800

    下拉加载更多和加载数据时的进度条 没有更多数据和数据为空时的提示

--- lib/app/modules/productList/views/product_list_view.dart
+++ lib/app/modules/productList/views/product_list_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:xmeshop/app/modules/productList/controllers/product_list_controller.dart';
@@ -30,75 +31,92 @@ class ProductListView extends GetView<ProductListController>{
     );
   }
 
+  Widget _buildProgressIndicator(){
+    if(controller.hasData.value){
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }else{
+      return const Center(
+        child: Text("没有数据了喔，我是有底线的"),
+      );
+    }
+  }
+
   Widget _buildProductList(){
-    return Obx(()=>ListView.builder(
+    return Obx(()=>controller.pList.isNotEmpty? ListView.builder(
         controller: controller.scrollController,
         padding: EdgeInsets.fromLTRB(ScreenAdapter.height(26), ScreenAdapter.height(140), ScreenAdapter.height(26), ScreenAdapter.height(26)),
         itemCount: controller.pList.length,
         itemBuilder: (context, index){
           PListModelItem item = controller.pList[index];
-          return Container(
-            // color: Colors.white,
-            margin: EdgeInsets.only(bottom: ScreenAdapter.height(26)),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Row(
-              children: [
-                Container(
-                  padding: EdgeInsets.all(ScreenAdapter.width(60)),
-                  width: ScreenAdapter.width(400),
-                  height: ScreenAdapter.height(460),
-                  child: Image.network("${HttpClient.replaceUri(item.pic)}",fit: BoxFit.cover,),
-                  // child: Text("11111111111111111"),
+          return Column(
+            children: [
+              Container(
+                // color: Colors.white,
+                margin: EdgeInsets.only(bottom: ScreenAdapter.height(26)),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(bottom: ScreenAdapter.width(20)),
-                        child: Text(item.title, style: TextStyle(fontSize: ScreenAdapter.fontSize(42), fontWeight: FontWeight.bold),),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(bottom: ScreenAdapter.width(20)),
-                        child: Text(item.subTitle, style: TextStyle(fontSize: ScreenAdapter.fontSize(34),),),
-                      ),
-                      Container(
-                        padding: EdgeInsets.only(bottom: ScreenAdapter.height(20)),
-                        child: Row(
-                          children: [
-                            Expanded(child: Column(
+                child: Row(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(ScreenAdapter.width(60)),
+                      width: ScreenAdapter.width(400),
+                      height: ScreenAdapter.height(460),
+                      child: Image.network("${HttpClient.replaceUri(item.pic)}",fit: BoxFit.cover,),
+                      // child: Text("11111111111111111"),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(bottom: ScreenAdapter.width(20)),
+                            child: Text(item.title, style: TextStyle(fontSize: ScreenAdapter.fontSize(42), fontWeight: FontWeight.bold),),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(bottom: ScreenAdapter.width(20)),
+                            child: Text(item.subTitle, style: TextStyle(fontSize: ScreenAdapter.fontSize(34),),),
+                          ),
+                          Container(
+                            padding: EdgeInsets.only(bottom: ScreenAdapter.height(20)),
+                            child: Row(
                               children: [
-                                Text("超大屏", style: TextStyle(fontSize: ScreenAdapter.fontSize(34),fontWeight: FontWeight.bold),),
-                                Text("6.1寸", style: TextStyle(fontSize: ScreenAdapter.fontSize(34),fontWeight: FontWeight.bold),),
+                                Expanded(child: Column(
+                                  children: [
+                                    Text("超大屏", style: TextStyle(fontSize: ScreenAdapter.fontSize(34),fontWeight: FontWeight.bold),),
+                                    Text("6.1寸", style: TextStyle(fontSize: ScreenAdapter.fontSize(34),fontWeight: FontWeight.bold),),
+                                  ],
+                                )),
+                                Expanded(child: Column(
+                                  children: [
+                                    Text("超大屏", style: TextStyle(fontSize: ScreenAdapter.fontSize(34),fontWeight: FontWeight.bold),),
+                                    Text("6.1寸", style: TextStyle(fontSize: ScreenAdapter.fontSize(34),fontWeight: FontWeight.bold),),
+                                  ],
+                                )),
+                                Expanded(child: Column(
+                                  children: [
+                                    Text("超大屏", style: TextStyle(fontSize: ScreenAdapter.fontSize(34),fontWeight: FontWeight.bold),),
+                                    Text("6.1寸", style: TextStyle(fontSize: ScreenAdapter.fontSize(34),fontWeight: FontWeight.bold),),
+                                  ],
+                                )),
                               ],
-                            )),
-                            Expanded(child: Column(
-                              children: [
-                                Text("超大屏", style: TextStyle(fontSize: ScreenAdapter.fontSize(34),fontWeight: FontWeight.bold),),
-                                Text("6.1寸", style: TextStyle(fontSize: ScreenAdapter.fontSize(34),fontWeight: FontWeight.bold),),
-                              ],
-                            )),
-                            Expanded(child: Column(
-                              children: [
-                                Text("超大屏", style: TextStyle(fontSize: ScreenAdapter.fontSize(34),fontWeight: FontWeight.bold),),
-                                Text("6.1寸", style: TextStyle(fontSize: ScreenAdapter.fontSize(34),fontWeight: FontWeight.bold),),
-                              ],
-                            )),
-                          ],
-                        ),
-                      ),
-                      Text("￥${item.price}起", style: TextStyle(fontSize: ScreenAdapter.fontSize(38), fontWeight: FontWeight.bold),),
+                            ),
+                          ),
+                          Text("￥${item.price}起", style: TextStyle(fontSize: ScreenAdapter.fontSize(38), fontWeight: FontWeight.bold),),
 
-                    ],
-                  ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              (index==controller.pList.length-1)?_buildProgressIndicator():const Text(""),
+            ],
           );
-        }));
+        }):_buildProgressIndicator());
   }
 
   Widget _buildSubHeader(){
