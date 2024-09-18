0ef98d15c855607300c7b9d7cd31a2db6b42930a
nicole <wangguilixiadong.163.com>
2024-09-19 03:49:55 +0800

    添加省心优惠后详情界面的透明渐变appbar

--- lib/app/modules/productContent/controllers/product_content_controller.dart
+++ lib/app/modules/productContent/controllers/product_content_controller.dart
@@ -1,10 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:get/get_rx/src/rx_types/rx_types.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 
 class ProductContentController extends GetxController{
 
+  final ScrollController scrollController = ScrollController();
+  RxDouble opcity=0.0.obs;
+
   @override
   void onInit() {
     super.onInit();
+    scrollControllerListener();
   }
 
   @override
@@ -15,5 +22,18 @@ class ProductContentController extends GetxController{
   @override
   void onClose() {
     super.onClose();
+  }
+
+  //监听滚动条滚动事件
+  void scrollControllerListener() {
+    scrollController.addListener(() {
+      if (scrollController.position.pixels <= 100) {
+        opcity.value=scrollController.position.pixels/100;
+        if (kDebugMode) {
+          print( opcity.value);
+        }
+        update();
+      }
+    });
   }
 }
