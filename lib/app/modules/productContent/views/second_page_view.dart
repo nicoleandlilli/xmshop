668b0bb02dd11d8aab0f796b3415dcabf0da2a0d
nicole <wangguilixiadong.163.com>
2024-09-20 02:40:55 +0800

    添加省心优惠后详情界面中详情部分加载数据

--- lib/app/modules/productContent/views/second_page_view.dart
+++ lib/app/modules/productContent/views/second_page_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../../services/screenAdapter.dart';
 import '../controllers/product_content_controller.dart';
+import 'package:flutter_html/flutter_html.dart';
 
 class SecondPageView extends GetView {
     final ProductContentController controller = Get.find();
@@ -13,11 +14,29 @@ class SecondPageView extends GetView {
     return Container(
               key: controller.gk2,
               alignment: Alignment.center,
-              width: ScreenAdapter.width(1080),
-              height: ScreenAdapter.height(2900),
-              color: Colors.blue,
       child: Column(
-        children: [subHeader()],
+        children: [subHeader(),
+          Obx(() => controller.selectedSubTabsIndex == 1
+              ? Container(
+              width: ScreenAdapter.width(1080),
+              child: Html(
+                data: controller.pcontent.value.content,
+                style: {
+                  "body": Style(backgroundColor: Colors.white),
+                  "p": Style(fontSize: FontSize.large),
+                },
+              ))
+              : Container(
+            width: ScreenAdapter.width(1080),
+            child: Html(
+              data: controller.pcontent.value.specs,
+              style: {
+                "body": Style(backgroundColor: Colors.white),
+                "p": Style(fontSize: FontSize.large),
+              },
+            ),
+          )),
+        ],
       ),
             );
   }
