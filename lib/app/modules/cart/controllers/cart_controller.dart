2c17d1a222140403e140d6926418bc895c986c30
nicole <wangguilixiadong.163.com>
2024-10-20 07:41:31 +0800

    再购买详情界面添加加入购物车后，在购物车界面实时更新数据 不同界面的数据实时更新

--- lib/app/modules/cart/controllers/cart_controller.dart
+++ lib/app/modules/cart/controllers/cart_controller.dart
@@ -14,7 +14,6 @@ class CartController extends GetxController{
       print("cart init");
     }
     super.onInit();
-    getCartListData();
   }
 
   @override
--- lib/app/modules/cart/views/cart_view.dart
+++ lib/app/modules/cart/views/cart_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:get/get_state_manager/src/simple/get_view.dart';
 import 'package:xmeshop/app/modules/cart/views/cart_item_num_view.dart';
@@ -24,65 +25,75 @@ class CartView extends GetView<CartController>{
           }, child: const Text("编辑"))
         ],
       ),
-      body: Obx(()=> controller.cartList.isNotEmpty?Stack(
-        children: [
-          ListView(
-            children: controller.cartList.map((value){
-              return CartItemView(value);
-            }).toList(),
-          ),
-          Positioned(
-              left: 0,
-              right: 0,
-              bottom: 0,
-              child:Container(
-                padding: EdgeInsets.only(right: ScreenAdapter.width(20)),
-                width: ScreenAdapter.width(1080),
-                height: ScreenAdapter.height(190),
-                decoration: BoxDecoration(
-                    border: Border(top: BorderSide (color: Color.fromARGB(178, 240, 236, 236),width: ScreenAdapter.height(2))),
-                    color: Colors.white
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
+      body: GetBuilder<CartController>(
+        initState: (state){
+          //获取购物车数据
+          controller.getCartListData();
+        },
+        init: controller,
+        builder: (controller){
+          return controller.cartList.isNotEmpty?Stack(
+            children: [
+              ListView(
+                padding: EdgeInsets.only(bottom: ScreenAdapter.height(200)),
+                children: controller.cartList.map((value){
+                  return CartItemView(value);
+                }).toList(),
+              ),
+              Positioned(
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  child:Container(
+                    padding: EdgeInsets.only(right: ScreenAdapter.width(20)),
+                    width: ScreenAdapter.width(1080),
+                    height: ScreenAdapter.height(190),
+                    decoration: BoxDecoration(
+                        border: Border(top: BorderSide (color: Color.fromARGB(178, 240, 236, 236),width: ScreenAdapter.height(2))),
+                        color: Colors.white
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Checkbox(value: true, onChanged:(value){
-                          print(value);
-                        }),const Text("全选")
+                        Row(
+                          children: [
+                            Checkbox(value: true, onChanged:(value){
+                              print(value);
+                            }),const Text("全选")
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text("合计: "),
+                            Text("¥98.9",style: TextStyle(
+                                fontSize: ScreenAdapter.fontSize(58),
+                                color: Colors.red
+                            )),
+                            SizedBox(width: ScreenAdapter.width(20)),
+                            ElevatedButton(
+                                style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all(
+                                        const Color.fromRGBO(255, 165, 0, 0.9)),
+                                    foregroundColor:
+                                    MaterialStateProperty.all(Colors.white),
+                                    shape: MaterialStateProperty.all(
+                                      // CircleBorder()
+                                        RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(10)))),
+
+                                onPressed: (){
+
+
+                                }, child: Text("结算")),
+                          ],
+                        )
                       ],
                     ),
-                    Row(
-                      children: [
-                        Text("合计: "),
-                        Text("¥98.9",style: TextStyle(
-                            fontSize: ScreenAdapter.fontSize(58),
-                            color: Colors.red
-                        )),
-                        SizedBox(width: ScreenAdapter.width(20)),
-                        ElevatedButton(
-                            style: ButtonStyle(
-                                backgroundColor: MaterialStateProperty.all(
-                                    const Color.fromRGBO(255, 165, 0, 0.9)),
-                                foregroundColor:
-                                MaterialStateProperty.all(Colors.white),
-                                shape: MaterialStateProperty.all(
-                                  // CircleBorder()
-                                    RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(10)))),
-
-                            onPressed: (){
-
-
-                            }, child: Text("结算")),
-                      ],
-                    )
-                  ],
-                ),
-              ))
-        ],
-      ):const Center(child: Text("购物车空空的")),),
+                  ))
+            ],
+          ):const Center(child: Text("购物车空空的"));
+        },
+      ),
     );
 
   }
