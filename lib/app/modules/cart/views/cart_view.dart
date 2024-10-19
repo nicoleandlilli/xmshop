b030da23b7bb96f27a00a5955dac5ec8f8e9ad3c
nicole <wangguilixiadong.163.com>
2024-10-20 03:53:15 +0800

    调整购物车数据为空时界面的数据

--- lib/app/modules/cart/views/cart_view.dart
+++ lib/app/modules/cart/views/cart_view.dart
@@ -24,26 +24,13 @@ class CartView extends GetView<CartController>{
           }, child: const Text("编辑"))
         ],
       ),
-      body: Stack(
+      body: Obx(()=> controller.cartList.isNotEmpty?Stack(
         children: [
-          Obx(()=> controller.cartList.isNotEmpty?ListView(
+          ListView(
             children: controller.cartList.map((value){
               return CartItemView(value);
             }).toList(),
-          )
-              :const Text("")),
-          // ListView(
-          //   children: const [
-          //     CartItemView(),
-          //     CartItemView(),
-          //     CartItemView(),
-          //     CartItemView(),
-          //     CartItemView(),
-          //     CartItemView(),
-          //     CartItemView(),
-          //     CartItemView(),
-          //   ],
-          // ),
+          ),
           Positioned(
               left: 0,
               right: 0,
@@ -95,94 +82,11 @@ class CartView extends GetView<CartController>{
                 ),
               ))
         ],
-      ),
+      ):const Center(child: Text("购物车空空的")),),
     );
 
   }
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       backgroundColor: Colors.white,
-  //       elevation: 0,
-  //       title: const Text('购物车'),
-  //       centerTitle: true,
-  //       actions: [
-  //         TextButton(onPressed: (){
-  //
-  //         }, child: const Text("编辑"))
-  //       ],
-  //     ),
-  //     body: Stack(
-  //       children: [
-  //         ListView(
-  //           children: const [
-  //             CartItemView(),
-  //             CartItemView(),
-  //             CartItemView(),
-  //             CartItemView(),
-  //             CartItemView(),
-  //             CartItemView(),
-  //             CartItemView(),
-  //             CartItemView(),
-  //           ],
-  //         ),
-  //         Positioned(
-  //             left: 0,
-  //             right: 0,
-  //             bottom: 0,
-  //             child:Container(
-  //               padding: EdgeInsets.only(right: ScreenAdapter.width(20)),
-  //               width: ScreenAdapter.width(1080),
-  //               height: ScreenAdapter.height(190),
-  //               decoration: BoxDecoration(
-  //                   border: Border(top: BorderSide (color: Color.fromARGB(178, 240, 236, 236),width: ScreenAdapter.height(2))),
-  //                   color: Colors.white
-  //               ),
-  //               child: Row(
-  //                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  //                 children: [
-  //                   Row(
-  //                     children: [
-  //                       Checkbox(value: true, onChanged:(value){
-  //                         print(value);
-  //                       }),const Text("全选")
-  //                     ],
-  //                   ),
-  //                   Row(
-  //                     children: [
-  //                       Text("合计: "),
-  //                       Text("¥98.9",style: TextStyle(
-  //                           fontSize: ScreenAdapter.fontSize(58),
-  //                           color: Colors.red
-  //                       )),
-  //                       SizedBox(width: ScreenAdapter.width(20)),
-  //                       ElevatedButton(
-  //                           style: ButtonStyle(
-  //                               backgroundColor: MaterialStateProperty.all(
-  //                                   const Color.fromRGBO(255, 165, 0, 0.9)),
-  //                               foregroundColor:
-  //                               MaterialStateProperty.all(Colors.white),
-  //                               shape: MaterialStateProperty.all(
-  //                                 // CircleBorder()
-  //                                   RoundedRectangleBorder(
-  //                                       borderRadius: BorderRadius.circular(10)))),
-  //
-  //                           onPressed: (){
-  //
-  //
-  //                           }, child: Text("结算")),
-  //                     ],
-  //                   )
-  //                 ],
-  //               ),
-  //             ))
-  //       ],
-  //     ),
-  //   );
-  //
-  // }
 
 
 }
