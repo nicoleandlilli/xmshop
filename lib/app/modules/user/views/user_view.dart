b081144364925109bb272cc5a47cfb55e44f3b33
nicole <wangguilixiadong.163.com>
2024-10-21 19:50:30 +0800

    实现用户界面用户头像、登录、注册的界面

--- lib/app/modules/user/views/user_view.dart
+++ lib/app/modules/user/views/user_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
+import 'package:xmeshop/app/services/screenAdapter.dart';
 
 import '../../../services/kee_alive_wrapper.dart';
 
@@ -17,6 +18,7 @@ class UserView extends GetView{
             centerTitle: true,
             actions: [
               Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   const Text("会员码"),
                   IconButton(onPressed: (){
@@ -34,8 +36,30 @@ class UserView extends GetView{
               ),
             ],
           ),
-          body: const Center(
-            child:  Text("UserView"),
+          body: ListView(
+            padding: EdgeInsets.all(ScreenAdapter.height(20)),
+            children: [
+              //用户头像  登录注册
+              Container(
+                child: Row(
+                  children: [
+                    SizedBox(width: ScreenAdapter.width(40),),
+                    SizedBox(
+                      height: ScreenAdapter.height(150),
+                      width: ScreenAdapter.width(150),
+                      child: CircleAvatar(
+                        radius: ScreenAdapter.width(75),
+                        backgroundImage: const AssetImage("assets/images/user.png"),
+                      ),
+                    ),
+                    SizedBox(width: ScreenAdapter.width(40),),
+                    Text("登录/注册", style: TextStyle(fontSize: ScreenAdapter.fontSize(46)),),
+                    SizedBox(width: ScreenAdapter.width(40),),
+                    Icon(Icons.arrow_forward_ios, size: ScreenAdapter.fontSize(34),color: Colors.black54,),
+                  ],
+                ),
+              ),
+            ],
           ),
         )
     );
