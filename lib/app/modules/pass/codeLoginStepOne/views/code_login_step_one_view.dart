0a5afa6f570c47fa9d2d95a52e5d5b4d68b51f82
nicole <wangguilixiadong.163.com>
2024-10-22 08:49:22 +0800

    实现密码登录第一个界面

--- lib/app/modules/pass/codeLoginStepOne/views/code_login_step_one_view.dart
+++ lib/app/modules/pass/codeLoginStepOne/views/code_login_step_one_view.dart
@@ -6,6 +6,9 @@ import 'package:xmeshop/app/modules/pass/codeLoginStepOne/controllers/code_login
 import '../../../../services/kee_alive_wrapper.dart';
 import '../../../../services/screenAdapter.dart';
 import '../../../../widget/logo.dart';
+import '../../../../widget/passButton.dart';
+import '../../../../widget/passTextField.dart';
+import '../../../../widget/userAgreement.dart';
 
 class CodeLoginStepOneView extends GetView<CodeLoginStepOneController>{
   const CodeLoginStepOneView({super.key});
@@ -20,28 +23,28 @@ class CodeLoginStepOneView extends GetView<CodeLoginStepOneController>{
 
       body: ListView(
         padding: EdgeInsets.all(ScreenAdapter.width(40)),
-        children: const [
+        children: [
           //logo
-          Logo(),
+          const Logo(),
 
-          // //输入手机号
-          // PassTextFiled(
-          //     hintText: "请输入手机号",
-          //     onChanged: (value) {
-          //       print(value);
-          //     }),
-          //
-          // PassTextFiled(
-          //     hintText: "密码",
-          //     onChanged: (value) {
-          //       print(value);
-          //     }),
-          // //用户协议
-          // const UserAgreement(),
-          // //登录按钮
-          // PassButton(text: "获取验证码", onPressed: (){
-          //   print("获取验证码");
-          // })
+          //输入手机号
+          PassTextFiled(
+              hintText: "请输入手机号",
+              onChanged: (value) {
+                print(value);
+              }),
+
+          PassTextFiled(
+              hintText: "密码",
+              onChanged: (value) {
+                print(value);
+              }),
+          //用户协议
+          const UserAgreement(),
+          //登录按钮
+          PassButton(text: "获取验证码", onPressed: (){
+            print("获取验证码");
+          })
 
         ],
       ),
