ba87c8e7c7b5ca4d8d337d7ff7ce43e633cc0bec
nicole <wangguilixiadong.163.com>
2024-10-22 08:00:20 +0800

    添加登录、注册所需要的各个界面

---
+++ lib/app/modules/pass/codeLoginStepOne/views/code_login_step_one_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+import 'package:xmeshop/app/modules/pass/codeLoginStepOne/controllers/code_login_step_one_controller.dart';
+
+import '../../../../services/kee_alive_wrapper.dart';
+
+class SampleView extends GetView<CodeLoginStepOneController>{
+  const SampleView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const KeepAliveWrapper(
+        child: Scaffold(
+          body: Center(
+            child:  Text("GiveView"),
+          ),
+        )
+    );
+  }
+
+}
