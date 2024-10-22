203e3b5d5e0764e73200943d3c25a702f0834753
nicole <wangguilixiadong.163.com>
2024-10-22 20:32:46 +0800

    实现其它方式登录手机号、密码登录的界面布局

--- lib/app/modules/pass/passLogin/views/pass_login_view.dart
+++ lib/app/modules/pass/passLogin/views/pass_login_view.dart
@@ -1,23 +1,66 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../../services/screenAdapter.dart';
+import '../../../../widget/logo.dart';
+import '../../../../widget/passButton.dart';
+import '../../../../widget/passTextField.dart';
+import '../../../../widget/userAgreement.dart';
 import '../controllers/pass_login_controller.dart';
 
 class PassLoginView extends GetView<PassLoginController> {
   const PassLoginView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text('PassLoginView'),
-        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        actions: [TextButton(onPressed: () {}, child: Text("帮助"))],
       ),
-      body: Center(
-        child: Text(
-          'PassLoginView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: ListView(
+        padding: EdgeInsets.all(ScreenAdapter.width(40)),
+        children: [
+          const Logo(),
+          //输入手机号
+          PassTextFiled(
+              hintText: "请输入手机号",
+              onChanged: (value) {
+                print(value);
+              }),
+
+          PassTextFiled(
+              hintText: "请输入密码",
+              onChanged: (value) {
+                print(value);
+              }),
+          //用户协议
+          const UserAgreement(),
+          //登录按钮
+          PassButton(
+              text: "获取验证码",
+              onPressed: () {
+                if (kDebugMode) {
+                  print("获取验证码");
+                }
+                Get.toNamed("/code-login-step-two");
+              }),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              TextButton(onPressed: (){
+
+              }, child: const Text("忘记密码")),
+              TextButton(onPressed: (){
+
+              }, child: Text("验证码登录"))
+            ],
+          )
+        ],
       ),
     );
   }
