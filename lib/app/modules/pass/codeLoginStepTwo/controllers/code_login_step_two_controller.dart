8599ad60f4c3ab5914d083e6798e50716fc4811f
nicole <wangguilixiadong.163.com>
2024-10-22 20:11:10 +0800

    登录第二个界面中添加第三方插件PinCodeFields六宫格验证码

--- lib/app/modules/pass/codeLoginStepTwo/controllers/code_login_step_two_controller.dart
+++ lib/app/modules/pass/codeLoginStepTwo/controllers/code_login_step_two_controller.dart
@@ -1,9 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class CodeLoginStepTwoController extends GetxController {
-  //TODO: Implement CodeLoginStepTwoController
 
-  final count = 0.obs;
+  final TextEditingController editingController = TextEditingController();
+
   @override
   void onInit() {
     super.onInit();
@@ -19,5 +20,4 @@ class CodeLoginStepTwoController extends GetxController {
     super.onClose();
   }
 
-  void increment() => count.value++;
 }
--- lib/app/modules/pass/codeLoginStepTwo/views/code_login_step_two_view.dart
+++ lib/app/modules/pass/codeLoginStepTwo/views/code_login_step_two_view.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../../services/screenAdapter.dart';
+import '../../../../widget/logo.dart';
 import '../controllers/code_login_step_two_controller.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 
 class CodeLoginStepTwoView extends GetView<CodeLoginStepTwoController> {
   const CodeLoginStepTwoView({super.key});
@@ -15,11 +18,45 @@ class CodeLoginStepTwoView extends GetView<CodeLoginStepTwoController> {
         title: const Text('手机号快速登录'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'CodeLoginStepTwoView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: ListView(
+        padding: EdgeInsets.all(ScreenAdapter.width(40)),
+        children: [
+          const Logo(),
+          Container(
+            margin: EdgeInsets.only(top: ScreenAdapter.height(60)),
+            padding: EdgeInsets.all(ScreenAdapter.width(40)),
+            child: PinCodeTextField(
+              length: 6,
+              obscureText: false,
+              animationType: AnimationType.fade,
+              pinTheme: PinTheme(
+                shape: PinCodeFieldShape.box,
+                borderRadius: BorderRadius.circular(5),
+                fieldHeight: 50,
+                fieldWidth: 40,
+                activeFillColor: Colors.white,
+              ),
+              animationDuration: const Duration(milliseconds: 300),
+              backgroundColor: Colors.blue.shade50,
+              enableActiveFill: true,
+              // errorAnimationController: errorController,
+              controller: controller.editingController,
+              onCompleted: (v) {
+                print("Completed");
+              },
+              onChanged: (value) {
+                print(value);
+
+              },
+              beforeTextPaste: (text) {
+                print("Allowing to paste $text");
+                //if you return true then it will show the paste confirmation dialog. Otherwise if false, then nothing will happen.
+                //but you can show anything you want here, like your pop up saying wrong paste format or etc
+                return true;
+              }, appContext: context, //注意需要传入context,
+            ),
+          )
+        ],
       ),
     );
   }
