592d2a92af7497e7a7bf062b660a12e0073a3b2b
nicole <wangguilixiadong.163.com>
2024-10-25 00:03:23 +0800

    实时获取收货地址页数据并渲染界面

--- lib/app/modules/address/addressList/controllers/address_list_controller.dart
+++ lib/app/modules/address/addressList/controllers/address_list_controller.dart
@@ -1,15 +1,43 @@
 import 'package:get/get.dart';
 
+import '../../../../models/address_model.dart';
+import '../../../../models/user_model.dart';
+import '../../../../services/http_client.dart';
+import '../../../../services/signServices.dart';
+import '../../../../services/userServices.dart';
+
 class AddressListController extends GetxController {
 
+  HttpsClient httpsClient = HttpsClient();
+  RxList<AddressItemModel> addressList=<AddressItemModel>[].obs;
+
   @override
-  void onInit() {    
+  void onInit() {
+    getAddressList();
     super.onInit();
   }
+
 
   @override
   void onClose() {
     super.onClose();
+  }
+
+  getAddressList() async {
+    List userList = await UserServices.getUserInfo();
+    UserModel userInfo = UserModel.fromJson(userList[0]);
+    Map tempJson = {"uid": userInfo.sId};
+    String sign = SignServices.getSign({
+      ...tempJson,
+      "salt": userInfo.salt //私钥
+    });
+    var response =
+    await httpsClient.get("/api/addressList?uid=${userInfo.sId}&sign=$sign");
+    if(response!=null){
+      var tempAddressList= AddressModel.fromJson(response.data);
+      addressList.value=tempAddressList.result!;
+      update();
+    }
   }
 
 }
